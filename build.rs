8e14357e74760be2b1725f0cc1274a24ac13f878
Nika Layzell <nika@thelayzells.com>
2019-10-20 23:55:17 -0400

    More precise rerun-if-changed

--- build.rs
+++ build.rs
@@ -2,15 +2,25 @@ use std::env;
 use std::error::Error;
 use std::process::Command;
 
-pub fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     // Set the `RUSTC` env var for use by the proc-macro.
     let rustc = env::var("RUSTC")?;
     println!("cargo:rustc-env=RUSTC={}", rustc);
 
+    // Re-run this script only if the `wasm` module is changed
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR")?;
+    println!(
+        "\
+cargo:rerun-if-changed={0}/wasm/Cargo.toml
+cargo:rerun-if-changed={0}/wasm/Cargo.lock
+cargo:rerun-if-changed={0}/wasm/src/lib.rs
+cargo:rerun-if-changed={0}/wasm/.cargo/config",
+        manifest_dir,
+    );
+
     // NOTE: The `.wasm` file would be sound to bundle, however the format of
     // `.rlib` files is unstable. Given that the rlibs for proc_macro2, syn, and
     // quote need to be built anyway, build everything.
-    let manifest_dir = env::var("CARGO_MANIFEST_DIR")?;
     let out_dir = env::var("OUT_DIR")?;
     let status = Command::new(env::var("CARGO")?)
         .args(&[
